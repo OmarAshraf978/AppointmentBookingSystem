f45bae5c19e2e98078a05f1dc695da8a5566474f
omar_Ashraf <omar_Ashraf@OMAR>
2026-07-21 17:37:37 +0300

    StateResponse

--- Application/Features/Appointments/Outputs/StateResponse.cs
+++ Application/Features/Appointments/Outputs/StateResponse.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Appointments.Outputs
 {
+    #region StateResponse
     public class StateResponse
     {
         public string Message { get; set; } = null!;
     }
+    #endregion
 }
