36b57b773ab9887e46c5c9c830c278ac8e865de6
omar_Ashraf <omar_Ashraf@OMAR>
2026-07-21 17:19:18 +0300

    GetAllServiceProvidersResponse

--- Application/Features/ServiceProviders/Outputs/GetAllServiceProvidersResponse.cs
+++ Application/Features/ServiceProviders/Outputs/GetAllServiceProvidersResponse.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Application.Features.ServiceProviders.Outputs
 {
+    #region GetAllServiceProvidersResponse
     public class GetAllServiceProvidersResponse
     {
         public int Id { get; set; }
@@ -17,4 +18,5 @@ namespace Application.Features.ServiceProviders.Outputs
         public double Longitude { get; set; }
         public List<ServiceResponse> AllServices { get; set; } = [];
     }
+    #endregion
 }
