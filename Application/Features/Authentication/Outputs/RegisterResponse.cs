a599fca6e2b26a3e7b758bf18c7de0adabeca47e
omar_Ashraf <omar_Ashraf@OMAR>
2026-07-21 16:31:16 +0300

    RegisterResponse

--- Application/Features/Authentication/Outputs/RegisterResponse.cs
+++ Application/Features/Authentication/Outputs/RegisterResponse.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Authentication.Outputs
 {
+    #region RegisterResponse
     public class RegisterResponse
     {
         public string DisplayName { get; set; } = null!;
@@ -14,4 +15,5 @@ namespace Application.Features.Authentication.Outputs
 
         public string Token { get; set; } = null!;
     }
+    #endregion
 }
