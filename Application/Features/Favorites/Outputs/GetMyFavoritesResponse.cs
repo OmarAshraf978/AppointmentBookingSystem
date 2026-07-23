2cc19abb9133f75b9d8f8cd35688dc085967f628
omar_Ashraf <omar_Ashraf@OMAR>
2026-07-23 21:04:31 +0300

    GetMyFavoritesResponse

--- Application/Features/Favorites/Outputs/GetMyFavoritesResponse.cs
+++ Application/Features/Favorites/Outputs/GetMyFavoritesResponse.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Favorites.Outputs
 {
+    #region GetMyFavoritesResponse
     public class GetMyFavoritesResponse
     {
         public int Id { get; set; }
         public int ServiceId { get; set; }
         public string ServiceName { get; set; } = null!;
     }
+    #endregion
 }
