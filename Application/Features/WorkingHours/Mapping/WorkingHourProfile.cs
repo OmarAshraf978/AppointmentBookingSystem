9a404aaaa9a32020b90c2417198608fd25a55270
omar_Ashraf <omar_Ashraf@OMAR>
2026-07-28 19:08:11 +0300

    WorkingHourProfile

--- Application/Features/WorkingHours/Mapping/WorkingHourProfile.cs
+++ Application/Features/WorkingHours/Mapping/WorkingHourProfile.cs
@@ -11,6 +11,7 @@ using Domain.Entities.Modules;
 
 namespace Application.Features.WorkingHours.Mapping
 {
+    #region WorkingHourProfile
     public class WorkingHourProfile : Profile
     {
         public WorkingHourProfile()
@@ -29,4 +30,5 @@ namespace Application.Features.WorkingHours.Mapping
                 .ForMember(dest => dest.ServiceProviderName, opt => opt.MapFrom(src => src.ServiceProvider.BusinessName));
         }
     }
+    #endregion
 }
